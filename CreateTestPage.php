adbea5c54d8524391dd504bc54da8531fdc8f361
Mohammed <ma.almeshal@gmail.com>
2020-07-17 14:45:56 +0300

    resolve merge 2

--- CreateTestPage.php
+++ CreateTestPage.php
@@ -13,24 +13,10 @@
 <body>
 
 
-    <div class="header">
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-           <a class="navbar-brand" href="#">ExamPlatform</a>
-           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon"></span>
-           </button>
-           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-             <div class="navbar-nav ml-auto nav-links">
-               
-               <a class="nav-item nav-link item" href="#">Home </a>
-               <a class="nav-item nav-link" href="#">Features</a>
-               <a class="nav-item nav-link " href="#">Pricing</a>
-               <a class="nav-item nav-link " href="#">Disabled</a>
-             </div>
-           </div>
-         </nav>
-       
+<?php
+require 'header.php';
+?>
+  
        
     </div>
 <div class="mainBox">
