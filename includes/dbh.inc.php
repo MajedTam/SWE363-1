b120bcac24fccad4fcfed2eb2fc51795f4b53299
Mohammed <ma.almeshal@gmail.com>
2020-07-17 13:19:36 +0300

    updated db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,15 @@
 <?php
 
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "examplatform";
+// $servername = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "examplatform";
+
+
+$servername = "sql12.freesqldatabase.com";
+$dbUsername = "sql12355404";
+$dbPassword = "AZt3eWtPaR";
+$dbName = "sql12355404";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
